cc80575c4393aa86b828b7acf545f986504f673d
prudd <prudd@192.168.50.1>
2013-05-03 03:50:41 -0400

    fixed NumExpr

--- src/cminuscompiler/NumExpression.java
+++ src/cminuscompiler/NumExpression.java
@@ -11,6 +11,7 @@ import lowlevel.Function;
  * @author Paul Marshall
  */
 public class NumExpression extends Expression {
+
     private int num;
 
     public int getNum() {
@@ -20,22 +21,24 @@ public class NumExpression extends Expression {
     public void setNum(int num) {
         this.num = num;
     }
-    
+
     @Override
-    public void genCode(Function func){
+    public void genCode(Function func) {
+        HashMap symbolTable = func.getTable();
+        tempReg = symbolTable.size();
         func.getTable().put(tempReg, tempReg);
     }
-    
+
     @Override
     public int getRegNum() {
         return this.tempReg;
     }
-    
+
     @Override
     public void print() {
         System.out.print(this.num);
     }
-    
+
     @Override
     public void printFile(BufferedWriter bw) {
         try {
@@ -44,7 +47,7 @@ public class NumExpression extends Expression {
             System.err.print("Error in NumExp printFile");
         }
     }
-    
+
     @Override
     public void printASTFile(BufferedWriter bw, int level) {
         try {
@@ -52,17 +55,17 @@ public class NumExpression extends Expression {
             for (int i = 0; i < level; i++) {
                 inset += "\t";
             }
-            
+
             bw.append(inset + "NumExpression(" + this.num + ")\n");
         } catch (IOException ex) {
             System.err.print("Error in NumExp printFile");
         }
     }
-    
+
     public NumExpression(int num) {
         this.num = num;
     }
-    
+
     public NumExpression() {
         this(0);
     }
