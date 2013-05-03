7337d2423072132a8894b36054482b40b030047e
Danny <Danny@Danny-Desktop>
2013-05-02 21:59:33 -0400

    Fixing returnOp

--- src/cminuscompiler/ReturnStatement.java
+++ src/cminuscompiler/ReturnStatement.java
@@ -32,11 +32,9 @@ public class ReturnStatement extends Statement {
         int regNum = expression.getRegNum();
         Operation assignOp = new Operation(Operation.OperationType.ASSIGN, currentBlock);
         assignOp.setSrcOperand(0, new Operand(OperandType.REGISTER, regNum));
-        assignOp.setDestOperand(0, new Operand(OperandType.REGISTER, "retreg"));
+        assignOp.setDestOperand(0, new Operand(OperandType.MACRO, "RetReg"));
+        currentBlock.appendOper(assignOp);
         
-        Operation returnOp = new Operation(Operation.OperationType.RETURN, currentBlock);
-        returnOp.setSrcOperand(0, new Operand(OperandType.REGISTER, "retreg"));
-        currentBlock.appendOper(returnOp);
     }
     
     @Override
