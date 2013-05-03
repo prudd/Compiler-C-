0fffc55364080569b9cb229d23c4283fdebd07bb
prudd <prudd@192.168.50.1>
2013-05-03 01:02:01 -0400

    genCode for BinExpression

--- src/cminuscompiler/BinaryExpression.java
+++ src/cminuscompiler/BinaryExpression.java
@@ -1,8 +1,13 @@
 package cminuscompiler;
 
+import compiler.CMinusCompiler;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import lowlevel.BasicBlock;
 import lowlevel.Function;
+import lowlevel.Operand;
+import lowlevel.Operation;
 
 /**
  *
@@ -37,12 +42,78 @@ public class BinaryExpression extends Expression {
     public void setOperator(String operator) {
         this.operator = operator;
     }
-    
+
     @Override
-    public void genCode(Function func){
-        
+    public void genCode(Function func) {
+        BasicBlock currentBlock = func.getCurrBlock();
+        HashMap symbolTable = CMinusCompiler.globalHash;
+        tempReg = symbolTable.size();
+        symbolTable.put(tempReg, tempReg);
+        leftExpression.genCode(func);
+        rightExpression.genCode(func);
+        if (operator.equals("<")) {
+            Operation ltOp = new Operation(Operation.OperationType.LT, currentBlock);
+            ltOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            ltOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            ltOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(ltOp);
+        } else if (operator.equals("<=")) {
+            Operation lteOp = new Operation(Operation.OperationType.LTE, currentBlock);
+            lteOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            lteOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            lteOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(lteOp);
+        } else if (operator.equals(">")) {
+            Operation gtOp = new Operation(Operation.OperationType.GT, currentBlock);
+            gtOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            gtOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            gtOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(gtOp);
+        } else if (operator.equals(">=")) {
+            Operation gteOp = new Operation(Operation.OperationType.GTE, currentBlock);
+            gteOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            gteOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            gteOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(gteOp);
+        } else if (operator.equals("==")) {
+            Operation equalOp = new Operation(Operation.OperationType.EQUAL, currentBlock);
+            equalOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            equalOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            equalOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(equalOp);
+        } else if (operator.equals("!=")) {
+            Operation notEQOp = new Operation(Operation.OperationType.NOT_EQUAL, currentBlock);
+            notEQOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            notEQOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            notEQOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(notEQOp);
+        } else if (operator.equals("+")) {
+            Operation addOp = new Operation(Operation.OperationType.ADD_I, currentBlock);
+            addOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            addOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            addOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(addOp);
+        } else if (operator.equals("-")) {
+            Operation subOp = new Operation(Operation.OperationType.SUB_I, currentBlock);
+            subOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            subOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            subOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(subOp);
+        } else if (operator.equals("*")) {
+            Operation mulOp = new Operation(Operation.OperationType.MUL_I, currentBlock);
+            mulOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            mulOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            mulOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(mulOp);
+        } else if (operator.equals("/")) {
+            Operation divOp = new Operation(Operation.OperationType.DIV_I, currentBlock);
+            divOp.setDestOperand(0, new Operand(Operand.OperandType.REGISTER, tempReg));
+            divOp.setSrcOperand(0, new Operand(Operand.OperandType.REGISTER, leftExpression.getRegNum()));
+            divOp.setSrcOperand(1, new Operand(Operand.OperandType.REGISTER, rightExpression.getRegNum()));
+            currentBlock.appendOper(divOp);
+        }
     }
-    
+
     @Override
     public int getRegNum() {
         return this.tempReg;
